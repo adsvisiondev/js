3f58522e39f1d9f50ce56fc5920e33d78dd03f23
D4vid <211244070+pageaval@users.noreply.github.com>
2026-07-06 12:08:10 +0330

    Update conf.js
    Signed-off-by: D4vid <211244070+pageaval@users.noreply.github.com>

--- conf.js
+++ conf.js
@@ -1,1 +1,84 @@
 
+Estonia 	vpn251233023.opengw.net:1688 	7 hours 	1 ms
+2 	Hong Kong 	v-dot-pn-hkg1.opengw.net 	7 days 	1 ms
+3 	Indonesia 	vpn902355403.opengw.net:1624 	6 days 	33 ms
+4 	Japan 	public-vpn-259.opengw.net 	41 days 	9 ms
+5 	Japan 	public-vpn-186.opengw.net 	41 days 	30 ms
+6 	Japan 	public-vpn-98.opengw.net 	41 days 	13 ms
+7 	Japan 	public-vpn-138.opengw.net 	41 days 	17 ms
+8 	Japan 	public-vpn-146.opengw.net 	41 days 	24 ms
+9 	Japan 	public-vpn-192.opengw.net 	41 days 	22 ms
+10 	Japan 	public-vpn-108.opengw.net 	41 days 	28 ms
+11 	Japan 	public-vpn-223.opengw.net 	41 days 	16 ms
+12 	Japan 	public-vpn-113.opengw.net 	41 days 	19 ms
+13 	Japan 	public-vpn-155.opengw.net 	41 days 	19 ms
+14 	Japan 	public-vpn-234.opengw.net 	41 days 	30 ms
+15 	Japan 	public-vpn-85.opengw.net 	41 days 	25 ms
+16 	Japan 	vpn497910382.opengw.net:1213 	8 days 	2 ms
+17 	Japan 	vpn154137087.opengw.net:1365 	61 days 	6 ms
+18 	Japan 	public-vpn-162.opengw.net 	41 days 	17 ms
+19 	Japan 	vpn102800483.opengw.net:1475 	14 days 	16 ms
+20 	Japan 	public-vpn-51.opengw.net 	41 days 	8 ms
+21 	Japan 	public-vpn-147.opengw.net 	41 days 	15 ms
+22 	Japan 	vpn914058380.opengw.net:1296 	2 days 	15 ms
+23 	Japan 	vpn105246560.opengw.net:1443 	1 days 	12 ms
+24 	Japan 	vpn346156339.opengw.net:1283 	23 hours 	16 ms
+25 	Japan 	vpn715779327.opengw.net:1472 	1 days 	43 ms
+26 	Japan 	vpn417812100.opengw.net:1862 	23 hours 	18 ms
+27 	Japan 	vpn652131424.opengw.net:1611 	7 hours 	11 ms
+28 	Japan 	vpn519380097.opengw.net:1217 	20 hours 	13 ms
+29 	Japan 	vpn559198341.opengw.net:1445 	7 hours 	2 ms
+30 	Japan 	vpn215888696.opengw.net:1342 	2 days 	4 ms
+31 	Japan 	vpn329807143.opengw.net:1879 	3 hours 	15 ms
+32 	Japan 	vpn454701555.opengw.net:1990 	0 mins 	8 ms
+33 	Japan 	vpn776694643.opengw.net:1964 	3 hours 	6 ms
+34 	Japan 	vpn809166095.opengw.net:1402 	0 mins 	6 ms
+35 	Japan 	vpn706570250.opengw.net:1206 	7 hours 	4 ms
+36 	Japan 	vpn340560924.opengw.net:1869 	0 mins 	16 ms
+37 	Japan 	vpn557548319.opengw.net:1498 	0 mins 	11 ms
+38 	Japan 	vpn441900948.opengw.net:1684 	0 mins 	3 ms
+39 	Japan 	vpn743905499.opengw.net:1403 	0 mins 	6 ms
+40 	Japan 	vpn813344574.opengw.net:1951 	7 hours 	26 ms
+41 	Japan 	vpn373583827.opengw.net:1869
+42 	Japan 	vpn700189518.opengw.net:1843 	0 mins 	19 ms
+43 	Japan 	vpn626684178.opengw.net:1564 	0 mins 	10 ms
+44 	Japan 	vpn352722593.opengw.net:1930 	0 mins 	4 ms
+45 	Japan 	vpn610928903.opengw.net:1663 	0 mins 	9 ms
+46 	Japan 	vpn715686587.opengw.net:1820 	0 mins 	26 ms
+47 	Japan 	vpn755967672.opengw.net:1731 	7 hours 	34 ms
+48 	Japan 	vpn835898425.opengw.net:1312 	3 hours 	35 ms
+49 	Republic of Korea 	vpn846611798.opengw.net:1846 	45 days 	24 ms
+50 	Republic of Korea 	vpn676301195.opengw.net:995 	12 days 	28 ms
+51 	Republic of Korea 	shooran.opengw.net 	89 days 	28 ms
+52 	Republic of Korea 	vpn931109776.opengw.net:1984 	4 days 	22 ms
+53 	Republic of Korea 	vpn672943141.opengw.net:1745 	1 days 	27 ms
+54 	Republic of Korea 	vpn692936902.opengw.net:1393 	2 days 	38 ms
+55 	Republic of Korea 	vpn535457630.opengw.net:1757 	1 days 	42 ms
+56 	Republic of Korea 	vpn999844931.opengw.net:995 	3 hours 	24 ms
+57 	Republic of Korea 	vpn815612783.opengw.net:1848 	7 hours 	28 ms
+58 	Republic of Korea 	vpn385346327.opengw.net:1934 	1 days 	50 ms
+59 	Republic of Korea 	vpn103963009.opengw.net:995 	7 hours 	28 ms
+60 	Republic of Korea 	vpn289788366.opengw.net:995 	3 hours 	27 ms
+61 	Republic of Korea 	vpn288796356.opengw.net:995 	0 mins 	26 ms
+62 	Republic of Korea 	vpn676594305.opengw.net:1418 	7 hours 	34 ms
+63 	Republic of Korea 	vpn908330665.opengw.net:1362 	0 mins 	28 ms
+64 	Republic of Korea 	vpn740752971.opengw.net:1782 	0 mins 	27 ms
+65 	Republic of Korea 	vpn219808599.opengw.net:995 	7 hours 	32 ms
+66 	Republic of Korea 	vpn720848404.opengw.net:1918 	3 hours 	28 ms
+67 	Republic of Korea 	vpn669570732.opengw.net:1548 	0 mins 	31 ms
+68 	Republic of Korea 	vpn693761884.opengw.net:1322 	0 mins 	33 ms
+69 	Republic of Korea 	vpn640710698.opengw.net:1589 	3 hours 	29 ms
+70 	Republic of Korea 	vpn632696211.opengw.net:1507 	3 hours 	35 ms
+71 	Republic of Korea 	vpn690933244.opengw.net:1687 	0 mins 	32 ms
+72 	Republic of Korea 	vpn703878682.opengw.net:1799 	0 mins 	30 ms
+73 	Republic of Korea 	vpn570228437.opengw.net:1749 	0 mins 	36 ms
+74 	Republic of Korea 	vpn615625770.opengw.net:995 	0 mins 	57 ms
+75 	Republic of Korea 	vpn385550111.opengw.net:1571 	10 days 	29 ms
+76 	Romania 	opengw.opengw.net 	15 days 	8 ms
+77 	Russian Federation 	vpn122714670.opengw.net:1283 	6 days 	-
+78 	Russian Federation 	vpn314520663.opengw.net:1371 	2 days 	120 ms
+79 	Russian Federation 	vpn883728371.opengw.net:1545 	0 mins 	4 ms
+80 	Russian Federation 	vpn782471270.opengw.net:1871 	3 hours 	121 ms
+81 	Russian Federation 	vpn368348825.opengw.net:1301 	0 mins 	46 ms
+82 	Vietnam 	vpn892603505.opengw.net:1316 	5 days 	21 ms
+83 	Vietnam 	vpn209052080.opengw.net:1576 	1 days 	44 ms
